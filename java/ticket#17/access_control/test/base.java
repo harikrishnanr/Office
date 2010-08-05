d1eea290aae53ddfe884d8b3f21e6de556a2a6b4
Harikrishnan.R <hari2981@yahool.com>
2010-08-05 18:02:28 +0530

    Deleted ticket#X

--- java/ticket#17/access_control/test/base.java
+++
@@ -1,6 +0,0 @@
-package test;
-
-public class base {
-       protected int i = 3;
-}
-
--- java/ticket#17/package/detective/pet.java
+++
@@ -1,8 +0,0 @@
-package detective;
-
-public class pet {
-
-	public pet() {
-		System.out.println("This is Pet");
-	}
-}
